912b8c2c676c73db0f94d6663f16fad0944ab25c
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-02-18 14:21:05 +0300

    Fix linter errors

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,14 @@ This is a simple Flask app that returns a string.
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
