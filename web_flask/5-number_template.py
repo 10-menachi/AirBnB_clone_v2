7c78e9d87602607094fdcb429f6175aa32ce1b80
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-02-20 15:02:10 +0300

    Complete task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+"""
+This is a simple Flask app that returns a string.
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Returns a string
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Returns a string
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Returns a string
+    """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """
+    Returns a string
+    """
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Displays n only if it is an integer
+    """
+    if isinstance(n, int):
+        return '%d is a number' % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Displays a template only if n is an integer
+    """
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
