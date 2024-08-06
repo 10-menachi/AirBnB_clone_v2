9e65c45e1a0c445229189e8d87bd6e78533e293f
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-08-06 05:15:52 +0300

    Create delete method in file storage and update all method

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- This script prepares a MySQL server for the project
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER IF NOT EXISTS 'hbnb_test_user'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test_user'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test_user'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
